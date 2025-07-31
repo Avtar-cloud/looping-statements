7c0f285481e7b1f4b85afb3dc6e9648fa17bf41f
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-07-31 10:08:52 +0530

    Create Print "SIT" 5 times using for loop.cpp

---
+++ Print "SIT" 5 times using for loop.cpp
@@ -0,0 +1,23 @@
+//Avtar singh
+//  entcA1 24070123027
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    a = "SIT";
+    for(int i=0;i<5;i++){
+        cout << a << endl;
+    }
+    return 0;
+}
+
+
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT      */
