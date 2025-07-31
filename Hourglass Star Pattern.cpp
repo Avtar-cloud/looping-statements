19b3788a404015805eed6493f99c4ba17d1530fd
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-07-31 10:06:59 +0530

    Create Hourglass Star Pattern.cpp

---
+++ Hourglass Star Pattern.cpp
@@ -0,0 +1,48 @@
+/* NAME  - Avtar singh
+   PRN   - 24070123027
+   CLASS - ENTC A-1
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+    int n=5;
+    for(i = 1; i<=n-1;i++){
+        
+        for(j=-1;j<i;j++){
+            cout << " ";
+        }
+            for(k=1;k<=n-i+1;k++){
+                cout << "* ";
+        }
+        cout << endl;
+    }
+    for(i = n; i>=1;i--){
+        for(j=1;j<=i+1;j++){
+            cout << " ";
+        }
+            for(k=1;k<=n-i+1;k++){
+                cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+/* OUTPUT
+  * * * * * 
+   * * * * 
+    * * * 
+     * * 
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * *    */
