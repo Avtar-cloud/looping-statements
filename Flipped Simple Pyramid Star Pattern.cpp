3e8a20aeab8fabd2af8b941ebacf18f5abf08815
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-07-31 10:04:15 +0530

    Create Flipped Simple Pyramid Star Pattern.cpp

---
+++ Flipped Simple Pyramid Star Pattern.cpp
@@ -0,0 +1,33 @@
+/* NAME  - Avtar singh
+   PRN   - 24070123027
+   CLASS - ENTC A-1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+    int n=5;
+    for(i = 1; i<=n;i++){
+        
+        for(j=1;j<n-i+1;j++){
+            cout << " ";
+        }
+            for(k=1;k<=i;k++){
+                cout << "*";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
+
+
+/* OUTPUT
+    *
+   **
+  ***
+ ****
+*****    */
